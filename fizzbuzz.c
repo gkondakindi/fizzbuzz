9b642be6a8f79ffd9da3360319a71681c97daa05
gkondakindi <kondakin@usc.edu>
2014-03-17 14:38:57 -0700

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main(void) 
 {
-	int i;
-	for(i=1;i<=100;i++)
+	int i=0;
+	for(i=1;i<=100;i++) //Assuming that the range of numbers to be displayed is 100
 	{
-		if(i%3==0)
+		if(i%3==0) //Checking if number is multiple of 3
 		{
-			if(i%5==0)
+			if(i%5==0) //Checking if number is multiple of 3 and 5
 			{
-				printf("fizzbuzz\n");
+				printf("fizzbuzz\n"); 
 				continue;
 			}
-			printf("fizz\n");
+			printf("fizz\n"); //Printed only when number is multiple of 3 and not multiple of 5
 		}
-		else if(i%5==0)
+		else if(i%5==0) //Checking if number is multiple of 5
 		{
 			printf("buzz\n");
 		}
-		else
+		else //If  number is neither a multiple of 3 or 5, the number is printed directly
 		{
 			printf("%d\n",i);
 		}
